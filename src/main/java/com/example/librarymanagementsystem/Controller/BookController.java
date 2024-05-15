6f68e60fbfe4870dc95278bd323c0b859436f838
aymanwaell <aymanwael@icloud.com>
2024-05-15 13:45:29 +0300

    add first api to get all books

--- src/main/java/com/example/librarymanagementsystem/Controller/BookController.java
+++ src/main/java/com/example/librarymanagementsystem/Controller/BookController.java
@@ -1,9 +1,13 @@
 package com.example.librarymanagementsystem.Controller;
 
 import com.example.librarymanagementsystem.Service.Impl.BookServiceImpl;
+import com.example.librarymanagementsystem.model.Book;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/books")
@@ -12,4 +16,8 @@ public class BookController {
     @Autowired
     private BookServiceImpl bookService;
 
+    @GetMapping("/")
+    public List<Book> getAllBooks(){
+        return bookService.getAllBooks();
+    }
 }
--- src/main/java/com/example/librarymanagementsystem/Service/Impl/BookServiceImpl.java
+++ src/main/java/com/example/librarymanagementsystem/Service/Impl/BookServiceImpl.java
@@ -2,8 +2,11 @@ package com.example.librarymanagementsystem.Service.Impl;
 
 import com.example.librarymanagementsystem.Repository.BookRepository;
 import com.example.librarymanagementsystem.Service.BookService;
+import com.example.librarymanagementsystem.model.Book;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class BookServiceImpl implements BookService {
@@ -11,4 +14,7 @@ public class BookServiceImpl implements BookService {
     @Autowired
     private BookRepository bookRepository;
 
+    public List<Book> getAllBooks() {
+        return bookRepository.findAll();
+    }
 }
